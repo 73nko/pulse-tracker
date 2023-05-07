55a90fc107bde15b0d3ee80e6eb04d521ac1d2f4
jcantosg <joselcgal@ext.inditex.com>
2023-05-08 00:11:37 +0200

    [pulsetracker-0002] Add save-metric endpoint

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,13 +1,19 @@
 import { utils } from '@pulse-tracker/utils';
-import express from 'express';
+import express from 'express'
+import cors from 'cors';
 import * as path from 'path';
 
 const app = express();
-
+app.use(cors());
+app.use(express.json());
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
   res.send({ message: utils() });
+});
+
+app.post('/save-metric', (req, res) => {
+  res.send({ message: 'Metric saved' });
 });
 
 const port = process.env.PORT || 3333;
